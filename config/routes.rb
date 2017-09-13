f3c49249153de9c9612ac18ae6cf357cf1582c2e
Bryce Wendelaar <brycewendelaar@InteraXons-MacBook-Pro.local>
2017-09-13 15:26:51 -0400

    Added pages controller for and home route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :campaigns do
     resources :pledges
   end
+
+  root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
